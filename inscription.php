104bda0454cbdb0f80708e81347e491b7b2e661d
Waxyspite2 <155013184+Waxyspite2@users.noreply.github.com>
2023-12-28 17:02:06 +0100

    inscription.php

---
+++ inscription.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "your_password";
+$dbname = "mydatabase";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Inscription réussie";
+    } else {
+        echo "Erreur d'inscription: " . $conn->error;
+    }
+}
+
+$conn->close();
+?>
